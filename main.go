de93c578302819f9c5c0bc45fe0d8ad19d3fb71d
Julio <juliopeguero@gmail.com>
2019-02-09 13:59:03 -0400

    port change

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8002", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
